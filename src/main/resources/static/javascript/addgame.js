2c8c305fa9407d69845edab0b1f41e09550f9519
MelissaJesmin <melissa.jesmin25@gmail.com>
2023-07-12 13:54:40 -0400

    Capstone

---
+++ src/main/resources/static/javascript/addgame.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('form');
+const gameThumbnail = document.getElementById('gameThumbnail');
+const gameName = document.getElementById('gameName');
+const gameGenre = document.getElementById('gameGenre');
+const gamePlatform = document.getElementById('gamePlatform');
+const gameCost = document.getElementById('#gameCost');
+
+async function addGame(obj) {
+    const response = await fetch(`${baseUrl}/form`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(obj)
+    }).catch(err => console.error(err.message))
+    if(response.status === 200) {
+        return getAllGames(userId);
+    }
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    let bodyObj = {
+         thumbnail: gameThumbnail.value,
+         title: gameName.value,
+         genre: gameGenre.value,
+         platform: gamePlatform.value,
+         cost: gameCost.value,
+    }
+    await addGame(bodyObj);
+
+    gameThumbnail.value = ''
+    gameName.value = ''
+    gameGenre.value = ''
+    gamePlatform.value = ''
+    gameCost.value = ''
+}
+
+
+form.addEventListener("submit", handleSubmit);
--- src/main/resources/static/javascript/game.js
+++ src/main/resources/static/javascript/game.js
@@ -1,9 +1,4 @@
-const form = document.querySelector('form');
-const gameThumbnail = document.getElementById('gameThumbnail');
-const gameName = document.getElementById('gameName');
-const gameGenre = document.getElementById('gameGenre');
-const gamePlatform = document.getElementById('gamePlatform');
-const gameCost = document.getElementById('#gameCost');
+
 
 const gameContainer = document.getElementById("displayGames");
 
@@ -20,16 +15,7 @@ async function getAllGames(userId) {
     }).then(res => res.json()).then(data => createGameCards(data)).catch(err => console.error(err))
 }
 
-async function addGame(obj) {
-    const response = await fetch(`${baseUrl}/form`, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify(obj)
-    }).catch(err => console.error(err.message))
-    if(response.status === 200) {
-        return getAllGames(userId);
-    }
-}
+
 const createGameCards = (game) => {
     gameContainer.innerHTML = "";
 
@@ -55,23 +41,3 @@ const createGameCards = (game) => {
 
 
 
-const handleSubmit = async (e) => {
-    e.preventDefault();
-    let bodyObj = {
-         thumbnail: gameThumbnail.value,
-         title: gameName.value,
-         genre: gameGenre.value,
-         platform: gamePlatform.value,
-         cost: gameCost.value,
-    }
-    await addGame(bodyObj);
-
-    gameThumbnail.value = ''
-    gameName.value = ''
-    gameGenre.value = ''
-    gamePlatform.value = ''
-    gameCost.value = ''
-}
-
-
-form.addEventListener("submit", handleSubmit);
